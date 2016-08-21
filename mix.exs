03e04fca870be2941876f68e78859a19509221ed
Nathan Wise <nathanwise@gmail.com>
2016-08-21 19:03:03 -0400

    Adding Markdown support for articles.

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule WiseideasBlog.Mixfile do
 
   def project do
     [app: :wiseideas_blog,
-     version: "0.1.0",
+     version: "0.1.1",
      elixir: "~> 1.3",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
@@ -41,6 +41,7 @@ defmodule WiseideasBlog.Mixfile do
      {:timex, "2.1.6"},
      {:edeliver, "~> 1.4.0" },
      {:exrm, "~> 1.0.3"},
+     {:earmark, "~> 1.0"},
      {:cowboy, "~> 1.0"}]
   end
 
