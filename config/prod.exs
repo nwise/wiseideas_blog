0407417d6df1416c564a3994e5ecc8df11ab7d56
Nathan Wise <nathanwise@gmail.com>
2016-08-21 14:40:22 -0400

    Le sigh.

--- config/prod.exs
+++ config/prod.exs
@@ -12,12 +12,12 @@ use Mix.Config
 # which you typically run after static files are built.
 config :wiseideas_blog, WiseideasBlog.Endpoint,
   http: [port: 8888],
-  url: [scheme: 'http', host: "blog.wise-ideas.com" ],
+  url: [host: "blog.wise-ideas.com", port: 8888],
   cache_static_manifest: "priv/static/manifest.json",
-  secret_key_base: System.get_env("SECRET_KEY_BASE")
+  secret_key_base: 'asdf'
 
 # Do not print debug messages in production
-config :logger, level: :info
+config :logger, level: :debug
 
 # ## SSL Support
 #
@@ -48,7 +48,7 @@ config :logger, level: :info
 # If you are doing OTP releases, you need to instruct Phoenix
 # to start the server for all endpoints:
 #
-#     config :phoenix, :serve_endpoints, true
+config :phoenix, :serve_endpoints, true
 #
 # Alternatively, you can configure exactly which server to
 # start per endpoint:
